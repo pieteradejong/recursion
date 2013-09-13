e752f977a3bdc4f2bb379239da24f39a0ee7915f
pieteradejong <pieter.a.dejong@gmail.com>
2013-09-13 14:09:45 -0700

    returns elements in array

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,7 +3,41 @@
 //   return document.getElementsByClassName(className);
 // };
 
+/*
+returns set of elements which all have the given class names
+
+uses:
+document.body
+element.childNodes
+element.classList
+
+*/
+
+
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
-};
+  
+
+
+  var walkDOM = function (element) {
+
+    var result = [];
+
+    if (element.childNodes.length === 0 && _.indexOf(element.classList, className) > -1) {
+        console.log("element pushed: \n");
+        console.log(element);
+        result.push(element);
+    }
+
+    for (var i = 0; i < element.childNodes.length; i++) {
+      
+      var child = element.childNodes[i];
+      var res = walkDOM(child);
+
+      if (res.length > 0) result.push(walkDOM(child));
+
+    }
+    return result;
+  }
+  return walkDOM(document.body);
+}
