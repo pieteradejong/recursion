2d4f121abad97b66fe78dbd6a1b91960c20e629f
pieteradejong <pieter.a.dejong@gmail.com>
2013-09-22 22:33:45 -0700

    done

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,41 +1,26 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
-
-/*
-returns set of elements which all have the given class names
-
-uses:
-document.body
-element.childNodes
-element.classList
-
-*/
-
-
-// But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
+
+  //save results in a closure outside the subroutine
+  
+  //create subroutine
+    //check if the current node contains the class if yes, push to result
+    //check if the current node has children; if yes recurse using subrouting through child nodes
+ //outside the sub routine return result
+  
+  var result = [];
   
   var walkDOM = function (element) {
 
-    var result = [];
-
-    if (element.childNodes.length === 0 && _.indexOf(element.classList, className) > -1) {
-        console.log("element pushed: \n");
-        console.log(element);
+    if (_.contains(element.classList, className)) {
         result.push(element);
     }
 
-    for (var i = 0; i < element.childNodes.length; i++) {
-      
-      var child = element.childNodes[i];
-      var res = walkDOM(child);
+      for (var i = 0; i < element.childNodes.length; i++) {
+          var child = element.childNodes[i];
+          walkDOM(child);
+      }
 
-      if (res.length > 0) result.push(walkDOM(child));
-
-    }
-    return result;
   }
-  return walkDOM(document.body);
+    walkDOM(document.body);
+    return result;
 }
